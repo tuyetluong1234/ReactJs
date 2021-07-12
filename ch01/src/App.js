3cb89f5c07bce38dcd0d32e184248dcf79c233e2
padi-dev-tuyetlt <tuyetlt@paditech.org>
2021-07-12 17:53:20 +0700

    update

--- ch01/src/App.js
+++ ch01/src/App.js
@@ -1,14 +1,45 @@
 
+import { Component } from 'react';
 import './App.css';
 import Course from './components/Course'
-function App() {
+class App extends Component {
+  render(){
+    const items =[
+      {
+        name:"ReactJS",
+        time:"30h",
+        free:true,
+        desc:"ReactJS is very simple !!"
+      },
+      {
+        name:"Angular",
+        time:"55h",
+        free:false
+      },
+      {
+        name:"NodeJs",
+        time:"35h",
+        free:true
+      }
+    ]
+    console.log(items);
+    const elmCourses = items.map((item, index) =>
+      // console.log(item)
+      <Course key={index} name={item.name} time={item.time} free={item.free}>{item.desc}</Course>
+    );
+    // const el = numbers.map((item) =>
+    //   <li>{item}</li>
+    // );
   return (
+    
     <div className="row">
-      <Course name="ReactJS" time="30h" free="true">ReactJS is very simple !!</Course>
-      <Course name="Angular" time="55h" free="false"></Course>
-      <Course name="NodeJs" time="35h" free="true"></Course>
+      {/* <Course name="ReactJS" time="30h" free={true}></Course>
+      <Course name="Angular" time="55h" free={false}></Course>
+      <Course name="NodeJs" time="35h" free={true}></Course> */}
+     {elmCourses}
     </div>
-  );
+  )
+  }
 }
 
 export default App;
--- ch01/src/components/Course.js
+++ ch01/src/components/Course.js
@@ -1,26 +1,41 @@
+import { Component } from 'react';
 import './../App.css';
 import Lesson from './Lesson';
-function Course(props) {
-  return (
-    <div className="col-md-4">
+class Course extends Component {
+  ShowButtonFree(){
+    const isFree = this.props.free;
+    if(isFree === true){
+      return <div className="card-footer">
+            <button className="btn btn-warning">View</button>
+          </div>
+      // console.log(isFree);
+    }
+    
+  }
+  render(){
+    return (
+      <div className="col-md-4">
         <div className="card">
           <div className="card-body">
-            <h5 className="card-title">{props.name}</h5>
+            <h5 className="card-title">{this.props.name}</h5>
             <div className="card-text">
-              <p>{props.time}</p>
-              <p>{props.children}</p>
+              <p>{this.props.time}</p>
+              <p>{this.props.children}</p>
               <ul className="list-group">
                 <Lesson></Lesson>
                 <Lesson></Lesson>
                 <Lesson></Lesson>
                 <Lesson></Lesson>
               </ul>
+              
             </div>
-            
           </div>
+          {this.ShowButtonFree()}
         </div>
       </div>
-  );
+    );
+  }
 }
+
 
 export default Course;
